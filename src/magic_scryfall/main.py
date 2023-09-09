ad1f7537ad0aa3deff913c81de526de1acca6527
Daniel Steimetz <dan.steimetz@gmail.com>
2023-09-09 14:51:33 +0000

    Alter config

--- src/magic_scryfall/main.py
+++ src/magic_scryfall/main.py
@@ -1,2 +1,14 @@
+from pprint import pprint
+
+import requests
+
+
 def main():
     print("Hello World")
+    response = requests.get("https://www.google.com")
+    print(response)
+    pprint(response.headers)
+
+
+if __name__ == "__main__":
+    main()
